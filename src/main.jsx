bf1804489612f994467b8869014aacdc2826f2ac
christo-zero-john <129399273+christo-zero-john@users.noreply.github.com>
2024-11-01 18:52:53 +0530

    Found new file,  src/main.jsx. Committing Via Global Bash Script:git-status-automated.bat

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import AppRoutes from "./AppRoutes";
+
+createRoot(document.getElementById("root")).render(<AppRoutes />);
