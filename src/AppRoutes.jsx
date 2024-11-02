11e31948f4fc1e90628e5d2989a3cb2b81e32d25
christo-zero-john <129399273+christo-zero-john@users.noreply.github.com>
2024-11-02 16:55:21 +0530

    Update AppRoutes.jsx

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -6,20 +6,21 @@ import Home from "./components/home/home";
 import "bootstrap/dist/css/bootstrap.min.css";
 import DemoPath from "./components/demo-path";
 import "./styles/defaults.css";
+import JoinUs from "./components/joinus/joinus";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/joinus" element={<DemoPath />} />
+        <Route path="/joinus" element={<JoinUs />} />
         <Route path="/social/:handle" element={<DemoPath />} />
         <Route path="/joinus/guide" element={<DemoPath />} />
         <Route path="/community/tasks" element={<DemoPath />} />
         <Route path="/community/interest-groups" element={<DemoPath />} />
         <Route path="/campus-chapter/execom" element={<DemoPath />} />
         <Route path="/community/faq" element={<DemoPath />} />
-        <Route path="/campus-chapter/events" element={<DemoPath />} />
+        <Route path="/campus-chapter/events" element={<DemoPath />} />z
       </Routes>
     </BrowserRouter>
   );
