c2e96ecda38aea53e208c555f9f944ffb109bb3d
christo-zero-john <129399273+christo-zero-john@users.noreply.github.com>
2024-11-03 20:39:28 +0530

    Found modified or deleted file,  src/components/get-started/get-started.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- src/components/get-started/get-started.jsx
+++ src/components/get-started/get-started.jsx
@@ -1,5 +1,21 @@
+import "../../styles/mu-guide.css";
+import Header from "./get-started-components/header";
+import NavBar from "../common/nav-bar";
+import Guide from "./get-started-components/guide";
+import Footer from "../common/footer";
+
 function GetStarted() {
-  return <div></div>;
+  return (
+    <div className="en-Oxanium">
+      <NavBar />
+      <h1 className="text-center text-secondary pt-5 pb-0">
+        Getting Started with <span className="en-mulearn">μlearn</span>
+      </h1>
+      <Header />
+      <Guide />
+      <Footer />
+    </div>
+  );
 }
 
 export default GetStarted;
