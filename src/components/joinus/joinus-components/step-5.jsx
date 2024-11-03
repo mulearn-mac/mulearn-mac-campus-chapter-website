a88b5b2926c922152245599b365a7436c29384fa
christo-zero-john <129399273+christo-zero-john@users.noreply.github.com>
2024-11-03 13:19:21 +0530

    Found new file,  src/components/joinus/joinus-components/step-5.jsx. Committing Via Global Bash Script:git-status-automated.bat

---
+++ src/components/joinus/joinus-components/step-5.jsx
@@ -0,0 +1,44 @@
+function Step5() {
+  return (
+    <div>
+      <h3 className="text-center p-4 bg-secondary text-light rounded">
+        5. Introduce yourself to the community.
+      </h3>
+      <p className="tips alert alert-info h6 my-2 py-2  font-weight-normal ">
+        Now it's time for you to introduce youself to the community.{" "}
+        <strong>
+          (Before that make sure your discord profile picture is updated to your
+          own photo. Your face must be clear in your profile picture.)
+        </strong>
+        <p className="mt-5">After changing your profile picture,</p> goto{" "}
+        <code>#self-introduction </code> channel and sent a message introducing
+        yourself including your <mark>name</mark>, <mark>college</mark>,{" "}
+        <mark>course</mark> and which <mark>year</mark> you are (eg: 2nd year
+        BCA), and your <mark>interests</mark>. Do not forget to include{" "}
+        <code>#ge-self-intro </code> hashtag.
+      </p>
+      <span>example message:</span> <br />
+      <code className="card bg-light border-info p-3 col-10 col-md-6 my-4">
+        #ge-self-intro <br />
+        Hello, <br />
+        I am Christo John, a second year BCA student from Mar Augusthinose
+        college, Ramapuram. <br /> Interested in web development, UiUx and
+        Artificial Intgelligence. Hope to learn from you all...
+      </code>
+      <p>
+        You're all set to embark on a journey of excellence. After this you may
+        find tasks that suits you and start doing them. You can find tasks in
+        the <code>lvl1-info</code>, <code>lvl2-info</code> and{" "}
+        <code>lvl3-info</code> channels in muLearn discord server. You should
+        gain atleast <strong>400 karma points from lvl2 tasks</strong> and{" "}
+        <strong>800 karma points from lvl3 tasks</strong>. Once you do get that
+        much, then only can you be considered as a true muLearn member, and get
+        access to lots of other chanels. More on that in the{" "}
+        <strong>muLearn a Beginners guide section</strong>. Before that you must
+        fill our <strong>Campus chapter Form</strong>.
+      </p>
+    </div>
+  );
+}
+
+export default Step5;
