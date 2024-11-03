15ee416e99100d84503504d849a8c09a30bf132c
christo-zero-john <129399273+christo-zero-john@users.noreply.github.com>
2024-11-03 20:39:33 +0530

    Found new file,  src/components/get-started/get-started-components/section-1.jsx. Committing Via Global Bash Script:git-status-automated.bat

---
+++ src/components/get-started/get-started-components/section-1.jsx
@@ -0,0 +1,28 @@
+function Section1() {
+  return (
+    <div>
+      <p className="Title text-orange fs-3 pt-4">μlearn Discord Server</p>
+
+      <p className="description">
+        Mulearn works inside discord. Discord is a platform used by numerous
+        communities (especially gaming communities) to hang out and coordinate.
+        Every community in discord is called a
+        <span className="mark">server</span>. Mulearn also has a discord server
+        of its own. mulearn uses <span className="mark">gamification</span> as a
+        tool that makes learning fun and engaging. Students gets{" "}
+        <span className="mark"> Karma Points</span> (Points in mulearn are
+        called as karma points . Just like experience points/exp in games.) for
+        completing tasks or making certain accomplishments. Karma points are a
+        proof of the work students do because these are earned by cracking tasks
+        designed by the best mentors out there. Karma points are worth their
+        weight in gold and can be used to obtain a variety of benefits,
+        including invitations to exclusive and paid events, promotions in our
+        community etc... There are certain internships that students having
+        karma points above a certain limit could only apply for.
+      </p>
+      <hr className="text-center w-50 mx-auto" />
+    </div>
+  );
+}
+
+export default Section1;
