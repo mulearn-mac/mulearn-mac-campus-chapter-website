efdb78a391e8aa75c7eddeaf43c6ac571d5d3b9b
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2024-11-17 13:58:49 +0530

    Create header.jsx

---
+++ src/components/execom/execom-components/header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <div>
+      {" "}
+      <div className="header">
+        <h1 className="header-text en-sirin">
+          <span className="execom-title">Execom</span>
+          <br />
+          <span className="title-year">2024-25</span>
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
